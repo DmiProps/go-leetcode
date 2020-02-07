9401dda2d471a283907f8c7c3099b35f66b026cf
dmiprops <dmi.develop@gmail.com>
2020-02-07 16:47:37 +0300

    Problem 8 resolved

--- problems/string-to-integer-atoi.go
+++ problems/string-to-integer-atoi.go
@@ -1,6 +1,10 @@
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // Problem8 run test problem
 func Problem8() {
@@ -21,6 +25,29 @@ func Problem8() {
 
 func myAtoi(str string) int {
 
-	return 0
+	str = strings.TrimLeft(str, " ")
+
+	var digits = "0123456789"
+	var end = len(str)
+
+	for idx, ch := range str {
+		if idx == 0 && (ch == '-' || ch == '+') {
+			continue
+		}
+		if strings.ContainsRune(digits, ch) {
+			continue
+		}
+		end = idx
+		break
+	}
+	str = str[:end]
+
+	i, _ := strconv.Atoi(str)
+	if i < -2147483648 {
+		i = -2147483648
+	} else if i > 2147483647 {
+		i = 2147483647
+	}
+	return i
 
 }
