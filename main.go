8552f7c88f069794f8f24787ec803e666e0e05ed
dmiprops <dmi.develop@gmail.com>
2020-04-02 10:04:55 +0300

    Resolve single-number

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 
-	problems.Problem14()
+	problems.ProblemDay1()
 
 }
---
+++ problems/single-number.go
@@ -0,0 +1,34 @@
+package problems
+
+import "fmt"
+
+// ProblemDay1...
+func ProblemDay1() {
+
+	test1 := []int{2, 2, 1}
+	fmt.Println(singleNumber(test1))
+
+	test2 := []int{4, 1, 2, 1, 2}
+	fmt.Println(singleNumber(test2))
+
+}
+
+func singleNumber(nums []int) int {
+
+	n := make(map[int]bool, len(nums))
+
+	for _, i := range nums {
+		_, ok := n[i]
+		if !ok {
+			n[i] = false
+		} else {
+			n[i] = true
+		}
+	}
+	for i, ok := range n {
+		if !ok {
+			return i
+		}
+	}
+	return 0
+}
