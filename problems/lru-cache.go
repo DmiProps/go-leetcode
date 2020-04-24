be88f5fb110d15aea64c4edd9545a84caa49f3c9
dmiprops <dmi.develop@gmail.com>
2020-04-24 23:08:16 +0300

    Test LRU Cache

--- problems/lru-cache.go
+++ problems/lru-cache.go
@@ -5,7 +5,23 @@ import "fmt"
 // ProblemX - LRU Cache
 func ProblemX() {
 
-	cache := Construct(2)
+	// test 1
+	cache := Construct(1)
+	fmt.Println(cache.Get(0)) // returns -1
+
+	// test 2
+	cache = Construct(2)
+
+	fmt.Println(cache.Get(2)) // returns -1
+	cache.Put(2, 6)
+	fmt.Println(cache.Get(1)) // returns -1
+	cache.Put(1, 5)
+	cache.Put(1, 2)
+	fmt.Println(cache.Get(1)) // returns 2
+	fmt.Println(cache.Get(2)) // returns 6
+
+	// test 3
+	cache = Construct(2)
 
 	cache.Put(1, 1)
 	cache.Put(2, 2)
@@ -16,6 +32,9 @@ func ProblemX() {
 	fmt.Println(cache.Get(1)) // returns -1 (not found)
 	fmt.Println(cache.Get(3)) // returns 3
 	fmt.Println(cache.Get(4)) // returns 4
+
+	// test 4
+	cache = Construct(2)
 
 	cache.Put(2, 1)
 	cache.Put(2, 2)
@@ -35,7 +54,7 @@ type LRUCache struct {
 // Construct - Construct this...
 func Construct(capacity int) LRUCache {
 
-	return LRUCache{M: make([][]int, capacity), C: capacity}
+	return LRUCache{M: make([][]int, 0, capacity), C: capacity}
 
 }
 
@@ -44,8 +63,15 @@ func (this *LRUCache) Get(key int) int {
 
 	for i, c := len(this.M)-1, 0; i >= 0 && c < this.C; i-- {
 		if this.M[i][0] == key {
-			this.M = append(this.M, []int{key, this.M[i][1]})
-			return this.M[i][1]
+			if c > 0 {
+				if i == 0 {
+					this.M = append(this.M[1:], []int{key, this.M[i][1]})
+				} else {
+					this.M = append(this.M[:i], this.M[i+1:]...)
+					this.M = append(this.M, []int{key, this.M[i][1]})
+				}
+			}
+			return this.M[len(this.M)-1][1]
 		}
 		c++
 	}
@@ -56,10 +82,30 @@ func (this *LRUCache) Get(key int) int {
 // Put - Put this...
 func (this *LRUCache) Put(key int, value int) {
 
-	this.M = append(this.M, []int{key, value})
-
-	if len(this.M) > this.C {
-		this.M = this.M[len(this.M)-this.C:]
+	if len(this.M) == 0 {
+		this.M = append(this.M, []int{key, value})
+	} else {
+		for i, c := len(this.M)-1, 0; i >= 0 && c < this.C; i-- {
+			if this.M[i][0] == key {
+				if c == 0 {
+					this.M[i][1] = value
+				} else {
+					if i == 0 {
+						this.M = append(this.M[1:], []int{key, value})
+					} else {
+						this.M = append(this.M[:i], this.M[i+1:]...)
+						this.M = append(this.M, []int{key, value})
+					}
+				}
+				return
+			}
+			c++
+		}
+		if len(this.M) < this.C {
+			this.M = append(this.M, []int{key, value})
+		} else {
+			this.M = append(this.M[1:], []int{key, value})
+		}
 	}
 
 }
