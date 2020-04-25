eb8abf2037640af25599b2394e266396ce74df91
dmiprops <dmi.develop@gmail.com>
2020-04-25 07:09:55 +0300

    Resolve LRU Cache

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ import "github.com/DmiProps/go-leetcode/problems"
 
 func main() {
 
-	problems.ProblemX()
+	problems.Problem146()
 
 }
--- problems/lru-cache.go
+++ problems/lru-cache.go
@@ -2,11 +2,30 @@ package problems
 
 import "fmt"
 
-// ProblemX - LRU Cache
-func ProblemX() {
+// Problem146 - 146. LRU Cache
+func Problem146() {
+
+	// test 0
+	//["LRUCache","put","put","put","put","get","get","get","get","put","get","get","get","get","get"]
+	//[[3],[1,1],[2,2],[3,3],[4,4],[4],[3],[2],[1],[5,5],[1],[2],[3],[4],[5]]
+	cache := Construct(3)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	cache.Put(3, 3)
+	cache.Put(4, 4)
+	fmt.Println(cache.Get(4)) // returns 4
+	fmt.Println(cache.Get(3)) // returns 3
+	fmt.Println(cache.Get(2)) // returns 2
+	fmt.Println(cache.Get(1)) // returns -1
+	cache.Put(5, 5)
+	fmt.Println(cache.Get(1)) // returns -1
+	fmt.Println(cache.Get(2)) // returns 2
+	fmt.Println(cache.Get(3)) // returns 3
+	fmt.Println(cache.Get(4)) // returns -1
+	fmt.Println(cache.Get(5)) // returns 5
 
 	// test 1
-	cache := Construct(1)
+	cache = Construct(1)
 	fmt.Println(cache.Get(0)) // returns -1
 
 	// test 2
@@ -54,26 +73,27 @@ type LRUCache struct {
 // Construct - Construct this...
 func Construct(capacity int) LRUCache {
 
-	return LRUCache{M: make([][]int, 0, capacity), C: capacity}
+	return LRUCache{M: make([][]int, capacity), C: capacity}
 
 }
 
 // Get - Get this...
 func (this *LRUCache) Get(key int) int {
 
-	for i, c := len(this.M)-1, 0; i >= 0 && c < this.C; i-- {
-		if this.M[i][0] == key {
-			if c > 0 {
-				if i == 0 {
-					this.M = append(this.M[1:], []int{key, this.M[i][1]})
-				} else {
-					this.M = append(this.M[:i], this.M[i+1:]...)
-					this.M = append(this.M, []int{key, this.M[i][1]})
-				}
-			}
-			return this.M[len(this.M)-1][1]
+	for i := 0; i < len(this.M); i++ {
+		if this.M[i] == nil {
+			break
 		}
-		c++
+		if this.M[i][0] == key {
+			ans := this.M[i][1]
+			if i > 0 {
+				for j := i; j > 0; j-- {
+					this.M[j] = this.M[j-1]
+				}
+				this.M[0] = []int{key, ans}
+			}
+			return ans
+		}
 	}
 	return -1
 
@@ -82,30 +102,25 @@ func (this *LRUCache) Get(key int) int {
 // Put - Put this...
 func (this *LRUCache) Put(key int, value int) {
 
-	if len(this.M) == 0 {
-		this.M = append(this.M, []int{key, value})
-	} else {
-		for i, c := len(this.M)-1, 0; i >= 0 && c < this.C; i-- {
-			if this.M[i][0] == key {
-				if c == 0 {
-					this.M[i][1] = value
-				} else {
-					if i == 0 {
-						this.M = append(this.M[1:], []int{key, value})
-					} else {
-						this.M = append(this.M[:i], this.M[i+1:]...)
-						this.M = append(this.M, []int{key, value})
-					}
-				}
-				return
-			}
-			c++
-		}
-		if len(this.M) < this.C {
-			this.M = append(this.M, []int{key, value})
-		} else {
-			this.M = append(this.M[1:], []int{key, value})
+	if this.M[0] != nil && this.M[0][0] == key {
+		this.M[0][1] = value
+		return
+	}
+
+	i := -1
+	for j := 1; j < len(this.M); j++ {
+		if this.M[j] == nil || this.M[j][0] == key {
+			i = j
+			break
 		}
 	}
+	if i > 0 {
+		for j := i; j > 0; j-- {
+			this.M[j] = this.M[j-1]
+		}
+	} else {
+		this.M = append([][]int{{key, value}}, this.M[:len(this.M)-1]...)
+	}
+	this.M[0] = []int{key, value}
 
 }
